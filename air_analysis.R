04b476eace000dc62042150719b6c885890ad768
Matt Bhagat-Conway <matt@indicatrix.org>
2026-04-07 13:16:59 -0400

    setup ci

--- air_analysis.R
+++ air_analysis.R
@@ -5,46 +5,14 @@
 # This data is extracted from the Bureau of Transportation Statistics DB1B dataset
 
 library(tidyverse)
+devtools::load_all()
+
+DATA_PATH = Sys.getenv("DATA_PATH")
 
 # first, we need to load the data
-data = read_csv("data/air_sample.csv")
+data = load_air_data(file.path(DATA_PATH, "air_sample.csv"), file.path(DATA_PATH, "L_CITY_MARKET_ID.csv"), file.path(DATA_PATH, "L_CARRIERS.csv"))
 
-# I always like to look at a sample of my data to see what I'm dealing with
-data[1:10, ]
-
-# The data have seven columns: origin and destination airport, origin and destination cities
-# carrier, and distance. The city and carrier are coded, so we will merge in other
-# (the airports have codes as well, but these are fairly well known - e.g. RDU is
-# Raleigh-Durham and LAX is Los Angeles; we won't match those with the official airport
-# names)
-
-market_ids = read_csv("data/L_CITY_MARKET_ID.csv")
-data = left_join(data, rename(market_ids, OriginCity = "Description"), by = c(OriginCityMarketID = "Code"))
-data = left_join(data, rename(market_ids, DestCity = "Description"), by = c(DestCityMarketID = "Code"))
-
-carriers = read_csv("data/L_CARRIERS.csv")
-data = left_join(data, rename(carriers, OperatingCarrierName = "Description"), by = c(OpCarrier = "Code"))
-data = left_join(data, rename(carriers, TicketingCarrierName = "Description"), by = c(TkCarrier = "Code"))
-
-# Now, we can see what the most popular air route is, by summing up the number of
-# passengers carried.
-pairs = data |>
-  group_by(Origin, Dest) |>
-  summarize(Passengers = sum(Passengers), distance_km = first(Distance) * 1.609)
-arrange(pairs, -Passengers)
-
-# we see that LAX-JFK (Los Angeles to New York Kennedy) is represented separately
-# from JFK-LAX. We'd like to combine these two. Create airport1 and airport2 fields
-# with the first and second airport in alphabetical order.
-pairs = pairs |>
-  mutate(
-    airport1 = if_else(Origin < Dest, Origin, Dest),
-    airport2 = if_else(Origin < Dest, Dest, Origin)
-  ) |>
-  group_by(airport1, airport2) |>
-  summarize(Passengers = sum(Passengers), distance_km = first(distance_km))
-
-arrange(pairs, -Passengers)
+busiest_routes(data, Origin, Dest)
 
 # This may be misleading, however, as some metropolitan areas have only one airport
 # (for example, Raleigh-Durham or Las Vegas), while others have more (for example,
@@ -52,20 +20,4 @@ arrange(pairs, -Passengers)
 # groups these airports together.
 # Now, we can see what the most popular air route is, by summing up the number of
 # passengers carried.
-pairs = data |>
-  group_by(OriginCity, DestCity) |>
-  summarize(
-    Passengers = sum(Passengers),
-    distance_km = first(Distance) * 1.609
-  ) |>
-  mutate(
-    city1 = if_else(OriginCity < DestCity, OriginCity, DestCity),
-    city2 = if_else(OriginCity < DestCity, DestCity, OriginCity)
-  ) |>
-  group_by(city1, city2) |>
-  summarize(
-    Passengers = sum(Passengers),
-    distance_km = first(distance_km) * 1.609
-  )
-
-arrange(pairs, -Passengers)
+busiest_routes(data, OriginCity, DestCity)
